ed0083a3d3ecf4b1fb69baef89f55c543ca81f4c
SandwichFox <johannes.ewerth@yahoo.com>
2023-01-31 13:35:37 +0100

    Initial commit for MBCord

---
+++ manual/mbcord/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+
+$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/SandwichFox/MBCord/releases/download/3.0.6/mbcord-Setup-3.0.6.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'mbcord*'
+  checksum      = 'AF87675F83F702359661E5D189E8DFE5A9D34CC4D11DB60919289348D57BA3E6'
+  checksumType  = 'sha256'
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "%USERPROFILE%/Desktop" -targetPath "%localappdata%/Programs/mbcord/mbcord.exe" [-description "A Discord rich presence client for Jellyfin and Emby"]
+Install-ChocolateyShortcut -shortcutFilePath "%AppData%\Microsoft\Windows\Start Menu\Programs" -targetPath "%localappdata%/Programs/mbcord/mbcord.exe" [-description "A Discord rich presence client for Jellyfin and Emby"]
+
+
+
+
+
